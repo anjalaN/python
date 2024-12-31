5ef800a34d26f808f590ff50ea7a3eb2e8f70838
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2024-12-31 12:35:22 +0100

    Add files via upload

---
+++ whatsyourname.py
@@ -0,0 +1,19 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    whatsyourname.py                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: arajapak <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/11/25 11:24:58 by arajapak          #+#    #+#              #
+#    Updated: 2024/11/25 11:25:02 by arajapak         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/python3
+#creerget and store variable with input
+fname = input("Hey, what's your first name?")
+lname = input("And your last name?")
+
+#appelle variable
+print ("Well, pleased to meet you " + fname + " " + lname)
