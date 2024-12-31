ca6458df1816426fdf1a953b8a9df921b760ed30
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2024-12-31 12:34:55 +0100

    Add files via upload

---
+++ name.py
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    name.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: arajapak <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/11/25 11:15:56 by arajapak          #+#    #+#              #
+#    Updated: 2024/11/25 11:16:18 by arajapak         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/python3
+#creer variable
+first_name = "Will"
+last_name = "42"
+
+#concaténation
+whole_name = first_name + " " +  last_name
+
+#appelle variable
+print(whole_name)
