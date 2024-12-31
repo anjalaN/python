505f4812a5fd2b41be17036488d4ddb0901af6d1
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2024-12-31 12:35:40 +0100

    Add files via upload

---
+++ iszero.py
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    iszero.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: arajapak <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/11/26 14:40:47 by arajapak          #+#    #+#              #
+#    Updated: 2024/11/26 14:41:14 by arajapak         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+#!/bin/python3
+
+#get variable 
+number = float(input("Please enter number: "))
+
+#compaire
+if number == 0 :
+    print("This number is equal to zero")
+
+else:
+    print("The number is different from zero ")
