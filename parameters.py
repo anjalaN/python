cf46298f21b0d7e73f9b963c1d6e0d60f49cfe98
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2024-12-31 12:40:56 +0100

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    parameters.py                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: arajapak <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/12/02 12:46:27 by arajapak          #+#    #+#              #
+#    Updated: 2024/12/02 12:46:32 by arajapak         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+arge_lenght=len(sys.argv)
+number_argv=arge_lenght-1
+print("Number of parameters: ",number_argv)
