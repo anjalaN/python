b58d747ec500042b3d19d868cc8896ac0235b4fd
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2024-12-31 12:43:52 +0100

    Add files via upload

---
+++ hello_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    hello_all.py                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: arajapak <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/12/06 16:48:03 by arajapak          #+#    #+#              #
+#    Updated: 2024/12/06 16:48:09 by arajapak         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+#!/bun/python3
+
+#creer funtion with def
+def hello():
+  print("Hello, everyone!")
+  
+#appelle funtion with nom de funtion hello
+hello()
+
