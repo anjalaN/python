12e617ed4782804a20ea9350428e44238b8d68ac
Rajapaksa Anjala <118836741+anjalaN@users.noreply.github.com>
2024-12-31 12:40:26 +0100

    Add files via upload

--- play_with_arrays.py
+++ play_with_arrays.py
@@ -6,15 +6,13 @@
 #                                                     +:+ +:+         +:+      #
 #    By: arajapak <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/12/02 12:44:06 by arajapak          #+#    #+#              #
-#    Updated: 2024/12/02 12:44:10 by arajapak         ###   ########.fr        #
+#    Created: 2024/12/02 12:44:51 by arajapak          #+#    #+#              #
+#    Updated: 2024/12/02 12:44:56 by arajapak         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
 import array 
 number_array =[2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [2 + item for item in number_array]
+print(number_array)
 
-print("Original array: ", number_array)
-print("New array: ",new_array)
-
+new_array = [2 + new_list for new_list in number_array if new_list> 5]
+print(new_array)
